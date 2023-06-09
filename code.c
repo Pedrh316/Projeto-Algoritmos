1e48a7cdbab9d9b58ce3de0a02ce465c224bcc8c
Pedro <pedrh315@gmail.com>
2023-06-09 01:01:15 -0300

    Código cliente, data e telefone

--- code.c
+++ code.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+/*
+Augusto Kenji Hirata Saraiva
+RA: 2564335
+*/
+struct Cliente {
+    char Nome[50];
+    int CPF[11];
+    char Telefone[9];
+    char Data[10];
+};
+int main() {
+
+    return 0;
+}
 #include <string.h>
 /*
     Pedro Henrique da Costa Silva
